f94f61a053cf1280eef12078230943027416d651
Florian Widder <florian.widder@live.de>
2018-11-26 10:09:35 +0100

    Add Example Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.addToList = this.addToList.bind(this);
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.clearList = this.clearList.bind(this);
+    this.state = {Liste : [], text : ''};
+  }
+
+  addToList(){
+    let newArray = this.state.Liste.slice();
+    newArray.push(this.state.text);
+    this.setState({Liste: newArray , text:''});
+  }
+
+  handleTextChange(event) {
+    this.setState({text: event.target.value});
+  }
+
+  clearList(){
+    this.setState({Liste: []});
+  }
+
   render() {
+    const listItems = this.state.Liste.map((text) =>
+      <li>{text}</li>
+    );
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type="text" placeholder="..." value={this.state.text} onChange={this.handleTextChange} />
+        <button onClick={this.addToList}>hinzufügen</button>
+        <ul>{listItems}</ul>
+        <button onClick={this.clearList}>Liste leeren</button>
       </div>
     );
   }
